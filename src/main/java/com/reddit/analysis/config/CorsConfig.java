65582ebe400d667afb3bc5cf5b6144c357e78299
Shrawan <shrawanre07@gmail.com>
2025-09-08 23:18:49 +0530

    Update cors

--- src/main/java/com/reddit/analysis/config/CorsConfig.java
+++ src/main/java/com/reddit/analysis/config/CorsConfig.java
@@ -11,7 +11,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://reddit-post-analysis.vercel.app/"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
